d8733b3229ad85a3cbaff32d29f6b4bedbf47d7e
Pule <pule@Pules-MacBook-Pro.local>
2021-11-04 04:02:13 +0700

    KOM-1-base-project umkm list layout map

--- src/routes/Home/Umkm/components/UmkmMaps.tsx
+++ src/routes/Home/Umkm/components/UmkmMaps.tsx
@@ -1,6 +1,8 @@
+import { ArrowRightOutlined } from "@ant-design/icons";
 import { Avatar, Button, Tag } from "antd";
 import React from "react";
 import { useSelector } from "react-redux";
+import { Link } from "react-router-dom";
 import Map from "../../../../components/Map/Map";
 import MapContainers from "../../../../components/Map/MapContainer";
 import { RootState } from "../../../../models/RootState";
@@ -39,15 +41,15 @@ const UmkmMaps: React.FC<Props> = ({ selectedId }) => {
     window.open(`https://wa.me/${no_hp}`, "_blank");
   };
   return (
-    <div style={{ zIndex: 1 }}>
-      <MapContainers zoom={12} width="100%" height="600px">
+    <div style={{ zIndex: 1, height: "600px", overflow: "hidden" }}>
+      <MapContainers zoom={12} width="100%" height="620px">
         {data?.map((item: any) => (
           <Map
             selectedId={Number(selectedId)}
             icon={<Avatar src={item?.profil_gambar} />}
             places={item}
           >
-            <h5>{capitalize(item.nama_umkm)}</h5>
+            <h5>{capitalize(item.nama_umkm)}</h5>{" "}
             <Tag color="blue" className="mb-1">
               {capitalize(item.umkm_detail?.klasifikasi_umkm)}
             </Tag>
@@ -62,6 +64,11 @@ const UmkmMaps: React.FC<Props> = ({ selectedId }) => {
             <a href={"mailto:" + item.email}>{item.email}</a>
             <h5 className="mt-1">Alamat:</h5>
             <h5>{capitalize(item?.umkm_detail?.alamat_umkm)}</h5>
+            <div className="peb-dflex-end">
+              <Link to={"/umkm/" + item.id}>
+                Lihat <ArrowRightOutlined />
+              </Link>
+            </div>
           </Map>
         ))}
       </MapContainers>
